9a1b3444f3c9e70129becb68003de3384423c606
Alberto <edhoru@hotmail.com>
2019-01-03 14:58:36 -0600

    Initial Commit

---
+++ Viper+DrawBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Viper+DrawBoard
+//
+//  Created by Alberto Huerdo on 1/3/19.
+//  Copyright © 2019 huerdo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Viper+DrawBoardTests/Viper_DrawBoardTests.swift
@@ -0,0 +1,34 @@
+//
+//  Viper_DrawBoardTests.swift
+//  Viper+DrawBoardTests
+//
+//  Created by Alberto Huerdo on 1/3/19.
+//  Copyright © 2019 huerdo. All rights reserved.
+//
+
+import XCTest
+@testable import Viper_DrawBoard
+
+class Viper_DrawBoardTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
